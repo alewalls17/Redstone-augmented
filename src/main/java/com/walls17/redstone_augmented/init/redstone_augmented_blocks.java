7eed02867e5c8fa9dfd442077ecab94b0db4b025
alewalls17 <74117272+alewalls17@users.noreply.github.com>
2020-11-08 08:05:54 -0600

    Update redstone_augmented_blocks.java

--- src/main/java/com/walls17/redstone_augmented/init/redstone_augmented_blocks.java
+++ src/main/java/com/walls17/redstone_augmented/init/redstone_augmented_blocks.java
@@ -1,7 +1,7 @@
 package com.walls17.redstone_augmented.init;
 
 import com.walls17.redstone_augmented.blocks.cardboard.cardboard;
-import com.walls17.redstone_augmented.blocks.pulse_decliner.pulse_decliner;
+
 import com.walls17.redstone_augmented.redstone_augmented;
 import net.minecraft.block.Block;
 import net.minecraftforge.fml.RegistryObject;
@@ -13,6 +13,5 @@ public class redstone_augmented_blocks {
 
     //blocks
     public static final RegistryObject<Block> CARDBOARD = BLOCKS.register("cardboard", cardboard::new);
-    public static final RegistryObject<Block> PULSE_DECLINER = BLOCKS.register("pulse_decliner", pulse_decliner::new);
 
 }
